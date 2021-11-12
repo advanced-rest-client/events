cf6df91185a9153df1701149c5c2be53cb95020d
Pawel Psztyc <jarrodek@gmail.com>
2021-11-12 15:30:12 -0800

    chore: updating the definition of the project
    Signed-off-by: Pawel Psztyc <jarrodek@gmail.com>

--- src/models/Project.d.ts
+++ src/models/Project.d.ts
@@ -1,14 +1,34 @@
 import { Entity } from './base';
 
-export declare interface Project {
+export declare interface Project extends ProjectFolder {
   /**
    * Project order
    */
   order?: number;
+  error?: boolean;
+}
+
+export declare interface ARCProject extends Project, Entity {
+}
+
+export interface ProjectFolder {
+  kind: 'ARC#Project' | 'ARC#ProjectFolder';
+  /**
+   * The name of the folder
+   */
+  name: string;
+  /**
+   * The description of the folder
+   */
+  description?: string;
   /**
    * List of requests associated with the project.
    */
   requests?: string[];
+  /**
+   * The list of sub-folders relative to this folder.
+   */
+  folders: ProjectFolder[];
   /**
    * Timestamp when the project was last updated.
    */
@@ -17,16 +37,4 @@ export declare interface Project {
    * Timestamp when the project was created.
    */
   created?: number;
-  /**
-   * The name of the project
-   */
-  name: string;
-  /**
-   * The description of the project
-   */
-  description?: string;
-  error?: boolean;
-}
-
-export declare interface ARCProject extends Project, Entity {
 }
