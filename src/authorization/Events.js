8d81a77d0f3e9e51cf9324ef2168f577e07d613e
Carolina Wright <carolina.wright@mulesoft.com>
2023-03-27 13:19:11 -0300

    fix: return oauth2 authorize result

--- src/authorization/Events.js
+++ src/authorization/Events.js
@@ -84,7 +84,7 @@ export class OidcRemoveTokensEvent extends CustomEvent {
 export async function authorizeOauth2Action(target, config) {
   const e = new OAuth2AuthorizeEvent(config);
   target.dispatchEvent(e);
-  return await e.detail.result;
+  return e.detail.result;
 }
 
 /**
